5a6c14a3fb5735513b9fcb7e97b487b85f3bab99
knightSarai <knight.sarai.dev@gmail.com>
2020-07-03 16:52:04 +0300

    form POST

--- src/pages/index.js
+++ src/pages/index.js
@@ -171,7 +171,8 @@ const Index = ({ data, location }) => {
         <h1 >CONSULTAION</h1>
         <p className="contact-text py-1">Don't hesitate leaving your info down below to schedule our first meeting together</p>
         <div className="form-container">
-          <form 
+          <form
+            action="POST" 
             name="contact"
             method="post"
             data-netlify = "true"
@@ -202,7 +203,7 @@ const Index = ({ data, location }) => {
             <label htmlFor="message">Message</label>
             <textarea name="message" id="message" placeholder="..."></textarea>
             </div>
-            <button className="form-btn">Send</button>
+            <button className="form-btn" type="submit">Send</button>
           </form>
           <div className="features">
             <div className="feature">
